1281cb81e88112c7ec14082ef35fc954cd2ef9c6
vlint <lintvadim@gmail.com>
2025-05-21 21:30:16 +0300

    Задание 1: написаны changeSets схем таблиц базы данных

---
+++ SpringAOP-Task1/src/main/java/ru/t1/lint/springaoptask1/SpringAopTask1Application.java
@@ -0,0 +1,13 @@
+package ru.t1.lint.springaoptask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopTask1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopTask1Application.class, args);
+    }
+
+}
---
+++ SpringAOP-Task1/src/test/java/ru/t1/lint/springaoptask1/SpringAopTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.t1.lint.springaoptask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopTask1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
