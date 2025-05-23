21a7e6eb2be90dd5633dcb50f4f7330833feab26
vlint <lintvadim@gmail.com>
2025-05-23 19:25:04 +0300

    Задание 1: добавлена миграция бд, .csv файлы с наполнением таблиц

---
+++ SpringAOP-Task1/src/main/java/ru/t1/lint/springaoptask1/repository/ClientRepository.java
@@ -0,0 +1,4 @@
+package ru.t1.lint.springaoptask1.repository;
+
+public interface ClientRepository {
+}
---
+++ SpringAOP-Task1/src/main/java/ru/t1/lint/springaoptask1/web/controller/RestExceptionHandler.java
@@ -0,0 +1,4 @@
+package ru.t1.lint.springaoptask1.web.controller;
+
+public class RestExceptionHandler {
+}
